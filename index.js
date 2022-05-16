f0b4a3e99aab0d67a13c08f9556befbfbc93d4f2
Dan <danilobomfim.si@gmail.com>
2022-05-16 00:58:46 -0300

    Finalizada a parte de validação e alguns ajustes na pagina.

--- index.js
+++ index.js
@@ -17,22 +17,54 @@ function validacao(valor) {
   document.getElementById("error").style.display = "none";
 
   //Etapa de validações.
+
+  //Verificando cpfs com todos os numeros iguais
   let valido = true;
+
   for (i in cpfsInvalidos) {
     if (cpfsInvalidos[i] === valor) {
       valido = false;
     }
   }
+
   //Verifica se é composto somente por numeros
   regex = /\d[0-9]{10,10}/;
   let result = valor.match(regex);
   let isNumbers = result != null ? true : false;
 
   if (valido && isNumbers) {
-    document.getElementById("success").style.display = "block";
-    console.log(`Valido? ${valido}`);
+    //Separando os digitos
+    let numeros = valor.substring(0, 9);
+    let digitos = valor.substring(9);
+
+    let pDigito = 0;
+    let sDigito = 0;
+    let cont = 0;
+    //Validando primeiro digito
+    for (let j = 10; j > 1; j--) {
+      pDigito += parseInt(numeros[cont]) * j;
+      cont++;
+    }
+    let digito = (pDigito * 10) % 11;
+    let digito1 = new String(digito > 9 ? 0 : digito);
+
+    //Validando segundo digito
+    cont = 0;
+    numeros = valor.substring(0, 10);
+    for (let h = 11; h > 1; h--) {
+      sDigito += parseInt(numeros[cont]) * h;
+      cont++;
+    }
+    digito = (sDigito * 10) % 11;
+    let digito2 = new String(digito > 9 ? 0 : digito);
+
+    //Apos validar os digitos retorna a informação para a tela
+    if (digito1 == digitos[0] && digito2 == digitos[1]) {
+      document.getElementById("success").style.display = "block";
+    } else {
+      document.getElementById("error").style.display = "block";
+    }
   } else {
     document.getElementById("error").style.display = "block";
-    console.log(`Valido? ${valido}`);
   }
 }
